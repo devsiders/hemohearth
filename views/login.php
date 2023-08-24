2f035087d0c537b44833e5f6b27a4014f0cae1c2
Carlos Torres <93625227+devsiders@users.noreply.github.com>
2023-08-24 10:19:27 -0500

    Update login.php

--- views/login.php
+++ views/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../img/favicon.png"/>
+    <link rel="icon" type="image/png" href="../Img/favicon.png"/>
     <title>HemoHearth</title>
     <link rel="stylesheet" href="../styles/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="index.html">
-                <img src="../img/favicon.png" width="60px" height="70px" class="navbar-brand" alt="">
+                <img src="../Img/favicon.png" width="60px" height="70px" class="navbar-brand" alt="">
                 HemoHearth</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
